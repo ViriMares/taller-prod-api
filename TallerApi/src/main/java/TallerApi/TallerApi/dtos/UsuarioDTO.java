995488626b0ab27cc6db9bfd1d76e5937f2aeacf
DiegoQuiroz420 <al02994936@tecmilenio.mx>
2025-04-02 18:34:56 -0600

    Cambios nuevos

---
+++ TallerApi/src/main/java/TallerApi/TallerApi/dtos/UsuarioDTO.java
@@ -0,0 +1,69 @@
+package TallerApi.TallerApi.dtos;
+
+public class UsuarioDTO {
+
+    private String nombreUsuario;
+    private String correoElectronico;
+    private String numeroTelefonico;
+    private String numeroSecundario;
+    private String direccion;
+    private String pais;
+    private String tipoUsuario;
+
+    // Getters y Setters
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getNumeroTelefonico() {
+        return numeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        this.numeroTelefonico = numeroTelefonico;
+    }
+
+    public String getNumeroSecundario() {
+        return numeroSecundario;
+    }
+
+    public void setNumeroSecundario(String numeroSecundario) {
+        this.numeroSecundario = numeroSecundario;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
