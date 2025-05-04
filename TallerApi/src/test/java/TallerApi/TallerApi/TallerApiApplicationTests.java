8d103d81ef7d3e568c1d33f9bd553e2ee07ad160
fridaarroyo399 <fridaarroyomaldonado@gmail.com>
2025-05-04 16:15:34 -0600

    Agrego pruebas unitarias

--- TallerApi/src/test/java/TallerApi/TallerApi/TallerApiApplicationTests.java
+++ TallerApi/src/test/java/TallerApi/TallerApi/TallerApiApplicationTests.java
@@ -8,6 +8,16 @@ class TallerApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Product dummyProduct = new Product(1L, "Camara", 10, 199.99);
+		when(service.findById(1L)).thenReturn(dummyProduct);
+
+		// Act
+		ResponseEntity<Product> response = controller.getById(1L);
+
+		// Assert
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertEquals("Camiseta", response.getBody().getName());
+		assertEquals(10, response.getBody().getStock());
 	}
 
 }
